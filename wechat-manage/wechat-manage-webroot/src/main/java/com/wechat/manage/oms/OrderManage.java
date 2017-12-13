980df74b14b1e9f5509b7ba4d6c7daefa8b56fa4
大智与大志 <251546843@qq.com>
2017-12-13 09:14:13 +0800

    删减部分代码

---
+++ wechat-manage/wechat-manage-webroot/src/main/java/com/wechat/manage/oms/OrderManage.java
@@ -0,0 +1,14 @@
+package com.wechat.manage.oms;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.weixin4j.User;
+
+public class OrderManage {
+	
+	/*@RequestMapping("/Users")
+	public List<User> getAllUser() {
+		
+	}*/
+}
